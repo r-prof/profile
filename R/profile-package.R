695dc8c1d2947c4f7bf69f4afb8f8ae341b395a9
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-16 17:09:13 +0100

    up package docs

--- R/profile-package.R
+++ R/profile-package.R
@@ -1,4 +1,7 @@
-#' @examples
+#' The profiler package defines a stable data format for profiler data,
+#' see [validate_profile()] for a definition.
+#' It supports reading and writing [Rprof] `.out` and
+#' [pprof](https://github.com/google/pprof) `.proto` files.
 #'
 #' @import rlang
 #' @import methods
