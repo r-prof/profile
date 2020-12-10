f385b4c9ffdeff59c426bce55c6fec295ffc90be
Kirill Müller <krlmlr+r@mailbox.org>
2020-12-10 04:57:44 +0100

    Refresh

--- tic.R
+++
@@ -1,6 +0,0 @@
-do_package_checks()
-
-# Build only for master or release branches
-if (ci_has_env("BUILD_PKGDOWN") && grepl("^master$|^r-|^docs$", ci_get_branch())) {
-  do_pkgdown(deploy = ci_can_push())
-}
